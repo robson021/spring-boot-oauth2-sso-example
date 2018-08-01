222e4cf629534e70a50e1edacbf130b071531d78
rob <nowak021@gmail.com>
2018-08-01 23:44:54 +0200

    remove autoconfig

--- boot2/src/main/java/com/example/demo/DemoApplication.java
+++ boot2/src/main/java/com/example/demo/DemoApplication.java
@@ -2,11 +2,12 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.reactive.ReactiveUserDetailsServiceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = ReactiveUserDetailsServiceAutoConfiguration.class)
 public class DemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
 }
